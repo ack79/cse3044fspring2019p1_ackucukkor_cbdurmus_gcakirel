8095a471ad032bd8aff35f6200abcc884b578448
Ahmet Can Küçükkör <ahmetcankucukkor@outlook.com>
2019-05-09 16:52:40 +0300

    Database script was created.

---
+++ libra_db.sql
@@ -0,0 +1,129 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.1.34-MariaDB)
+# Database: libra
+# Generation Time: 2019-05-09 13:50:37 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table advert
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `advert`;
+
+CREATE TABLE `advert` (
+  `advert_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `book_id` int(11) unsigned NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  `advert_desc` text COLLATE utf8_turkish_ci,
+  PRIMARY KEY (`advert_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table book
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `book`;
+
+CREATE TABLE `book` (
+  `book_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(255) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `book_author` varchar(255) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `book_desc` text COLLATE utf8_turkish_ci,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`book_id`),
+  CONSTRAINT `user_has_book` FOREIGN KEY (`book_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table friend
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `friend`;
+
+CREATE TABLE `friend` (
+  `first_user_id` int(11) unsigned NOT NULL,
+  `second_user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`first_user_id`),
+  KEY `user_has_friend2` (`second_user_id`),
+  CONSTRAINT `user_has_friend` FOREIGN KEY (`first_user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `user_has_friend2` FOREIGN KEY (`second_user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table message
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `message_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sender_user_id` int(11) unsigned NOT NULL,
+  `advert_id` int(11) unsigned NOT NULL,
+  `message_text` text COLLATE utf8_turkish_ci NOT NULL,
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table post
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `post_text` varchar(240) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`post_id`),
+  CONSTRAINT `user_has_post` FOREIGN KEY (`post_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `password` varchar(16) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `user_bio` varchar(240) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `user_img` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
