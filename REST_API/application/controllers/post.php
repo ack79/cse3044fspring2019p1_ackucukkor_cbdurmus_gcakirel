9ae8a06be9e80fbc8ec7aa17c1caab020df5ada3
Ahmet Can Küçükkör <ahmetcankucukkor@gmail.com>
2019-05-11 00:11:11 +0300

    Timeline endpoint was created.

--- REST_API/application/controllers/post.php
+++ REST_API/application/controllers/post.php
@@ -38,6 +38,37 @@ class Post extends CI_Controller {
 		
 	}
 
+	public function getTimeline(){
+		$user_id = $this->input->post('user_id');
+
+		if ($user_id == null ) {
+			$this->errorResponse("Eksik parametre girdiniz.");
+			exit();
+		}
+
+		$data = array(
+	        'user_id' => $user_id
+		);
+
+		$this->load->model('post_model');
+		$result = $this->post_model->getTimeline($data);
+
+		if ($result == FALSE) {
+			$this->errorResponse("DATABASE ERROR");
+			exit();
+		}
+		else{
+			header('Access-Control-Allow-Origin: *');
+			header('Content-type: application/json');
+
+			$response = new stdClass();
+			$response->success = TRUE;
+			$response->data = $result;
+		
+			echo json_encode($response,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+		}
+	}
+
 	private function errorResponse($errorMessage="Something went wrong."){
 		header('Access-Control-Allow-Origin: *');
 		header('Content-type: application/json');
--- REST_API/application/models/post_model.php
+++ REST_API/application/models/post_model.php
@@ -15,4 +15,33 @@ class Post_model extends CI_Model{
 		}
 	}
 
+	public function getTimeline($data){
+		$getFriends = $this->db->query('SELECT * FROM friend WHERE first_user_id = '. $data['user_id'] .' OR second_user_id = '. $data['user_id'])->result_array();
+		$friendIDArray = [$data['user_id']];
+		foreach ($getFriends as $row) {
+			if ($row['first_user_id'] != $data['user_id']) {
+				array_push($friendIDArray, $row['first_user_id']);
+			}
+			elseif ($row['second_user_id'] != $data['user_id']) {
+				array_push($friendIDArray, $row['second_user_id']);
+			}		
+		}
+
+		$this->db->or_where_in('user_id', $friendIDArray);
+		$this->db->order_by('created_date', 'DESC');
+		$timeline = $this->db->get('post')->result_array();
+		
+		if (empty($timeline)) {
+			return FALSE;
+		}
+		else{
+			$this->db->where_in('user_id', $friendIDArray);
+			$userInfo = $this->db->get('user')->result_array();
+			$postAndUser = ["post" => $timeline, "user" => $userInfo];
+			return $postAndUser;
+		}
+
+
+	}
+
 }
